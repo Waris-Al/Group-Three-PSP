8fc0ab86fff7696dfbdb98b03bd90b3a27ce0fad
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2023-02-06 11:58:13 +0000

    self-audit has a progress bar, still needs formatting

--- SelfAudit.php
+++ SelfAudit.php
@@ -20,11 +20,18 @@
   <h1>10 Point Checklist</h1>
   <form>
     <ul>
-      <?php 
-        for ($i = 1; $i <= 10; $i++) {
-          echo "<li>Task $i<input type='checkbox' id='task-$i' name='task-$i' value='done'></li>";
-        }
-      ?>
+    <li>Accessibility Tab on the Homepage <input type='radio' id='task-0-yes' name='task-0' value='yes'>Yes<input type='radio' id='task-0-no' name='task-0' value='no'>No</li>
+      <li>Videos available on website <input type='radio' id='task-1-yes' name='task-1' value='yes'>Yes<input type='radio' id='task-1-no' name='task-1' value='no'>No</li>
+      <li>Audio description of video available <input type='radio' id='task-2-yes' name='task-2' value='yes'>Yes<input type='radio' id='task-2-no' name='task-2' value='no'>No</li>
+      <li>Link to online accessibility guide <input type='radio' id='task-3-yes' name='task-3' value='yes'>Yes<input type='radio' id='task-3-no' name='task-3' value='no'>No</li>
+      <li>Audio version of accessibility guide available <input type='radio' id='task-4-yes' name='task-4' value='yes'>Yes<input type='radio' id='task-4-no' name='task-4' value='no'>No</li>
+      <li>Tab on the Homepage <input type='radio' id='task-5-yes' name='task-5' value='yes'>Yes<input type='radio' id='task-5-no' name='task-5' value='no'>No</li>
+      <li>Wi-Fi code displayed <input type='radio' id='task-6-yes' name='task-6' value='yes'>Yes<input type='radio' id='task-6-no' name='task-6' value='no'>No</li>
+      <li>Information on concession available <input type='radio' id='task-7-yes' name='task-7' value='yes'>Yes<input type='radio' id='task-7-no' name='task-7' value='no'>No</li>
+      <li>Adjustable text/contrast on the website <input type='radio' id='task-8-yes' name='task-8' value='yes'>Yes<input type='radio' id='task-8-no' name='task-8' value='no'>No</li>
+      <li>Accessible On-Site Parking <input type='radio' id='task-8-yes' name='task-8' value='yes'>Yes<input type='radio' id='task-8-no' name='task-8' value='no'>No</li>
+         
+        
     </ul>
   </form>
   <div class="progress">
@@ -35,8 +42,8 @@
   </div>
   <script>
     $(document).ready(function() {
-      $(':checkbox').change(function() {
-        var totalChecked = $(':checkbox:checked').length;
+      $(':radio').change(function() {
+        var totalChecked = $(':radio:checked').length;
         var percentage = (totalChecked / 10) * 100;
         $('.progress-bar').css('width', percentage + '%');
         $('.progress-bar').text(percentage + '%');
@@ -51,3 +58,4 @@
 
 
 <?php include("Footer.php") ?>
+
