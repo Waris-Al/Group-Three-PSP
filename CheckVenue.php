cf5b12570361a3d66118ce654f0c209a82e10981
Waris-Al <92109525+Waris-Al@users.noreply.github.com>
2023-02-15 13:20:47 +0000

    attemping to bring it all together, converting mysqli to sqlite3, checkvenue needs to be worked out but rest all seem to be working as an overall product

--- CheckVenue.php
+++ CheckVenue.php
@@ -1,7 +1,166 @@
-<?php include("NavigationBar.php") ?>
+<?php
+//include 'connect_test_db.php';
+session_start();
+include("NavigationBar.php");
+
+$searchErr = '';
+$building='';
+if(isset($_POST['save']))
+{
+    if(!empty($_POST['search']))
+    {
+        $search = $_POST['search'];
+        $stmt = $con->prepare("select * from company where city like '%$search%' or btype like '%$search%'");
+        $stmt->execute();
+        $building = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($building);
+         
+    }else if(!empty($_POST['search1'])){
+
+        $search = $_POST['search1'];
+        $stmt = $con->prepare("select * from company where city like '%$search%' or btype like '%$search%'");
+        $stmt->execute();
+        $building = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }else if(!empty($_POST['search2'])){
+
+      $search = $_POST['search2'];
+
+    $wchair = "wchair";
+    $hearing = "hearing";
+    $parking = "parking";
+    $check = "yes";
+
+      if(strcmp($search,$wchair) == 0){
+        $stmt = $con->prepare("select * from company where id IN(select cid from questions where wchair like '%$check%')");
+        $stmt->execute();
+        $building = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      }else if (strcmp($search,$hearing) == 0){
+
+        $stmt = $con->prepare("select * from company where id IN(select cid from questions where hearing like '%$check%')");
+        $stmt->execute();
+        $building = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      }else if(strcmp($search,$parking) == 0){
+        $stmt = $con->prepare("select * from company where id IN(select cid from questions where parking like '%$check%')");
+        $stmt->execute();
+        $building = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+      
+  }
+    else
+    {
+        $searchErr = "Please enter the information";
+    }
+    
+}
+ 
+?>
+<html>
+<head>
+<title>A HUGE Welcome From Access For All</title>
+<link rel="stylesheet" href="bootstrap.css" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap-theme.css" crossorigin="anonymous">
+<style>
+.container{
+    width:70%;
+    height:30%;
+    padding:20px;
+}
+</style>
+</head>
+ 
+<body>
+    <div class="container">
+    <h2 class="mb-3">Search Filters</h2>
+    <br/><br/>
+    
+    <form class="form-vertical" action="#" method="post">
+    <div class="column">
+        <div class="form-group">
+            <label class="control-label col-sm-4" for="email"><b>Search with keywords:</b>:</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" name="search" placeholder="search here">
+            </div><br/>
+
+            <div class="col-md-4">
+              <label for="Business-type">Business Type</label>
+              <select class="form-select" name="search1">
+                <option value="">All</option>
+                <option value="restaurant">Restaurant</option>
+                <option value="venue">Venue</option>
+                <option value="cinema">Cinema</option>
+                <option value="gym">Gym</option>
+              </select>
+            </div>
 
 
+            <div class="col-md-4">
+              <label for="disability-type">Disability Type</label>
+              <select class="form-select" name="search2">
+                <option value="">All</option>
+                <option value="hearing">Hearing</option>
+                <option value="wchair">Wheel Chair</option>
+                <option value="parking">Parking</option>
+              </select>
+
+              </div>
+            <div class="col-sm-2">
+              <button type="submit" name="save" class="btn btn-success btn-sm">Submit</button>
+            </div>
+        </div>
+        <div class="form-group">
+            <span class="error" style="color:red;">* <?php echo $searchErr;?></span>
+        </div>
+         
+    </div>
+    </form>
+    <br/><br/>
 
 
-
-<?php include("Footer.php") ?>
+    <br/>
+    <h3><u>Search Result</u></h3><br/>
+    <div class="table-responsive">          
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Company Name</th>
+            <th>City</th>
+            <th>Postal</th>
+            <th>Business Type</th>
+          </tr>
+        </thead>
+        <tbody>
+                <?php
+                 if(!$building)
+                 {
+                    echo '<tr>No data found</tr>';
+                 }
+                 else{
+                    foreach($building as $key=>$value)
+                    {
+                        ?>
+                    <tr>
+                        <td><?php echo $key+1;?></td>
+                        <td><?php echo $value['cname'];?></td>
+                        <td><?php echo $value['city'];?></td>
+                        <td><?php echo $value['postal'];?></td>
+                        <td><?php echo $value['btype'];?></td>
+                    </tr>
+                         
+                        <?php
+                    }
+                     
+                 }
+                ?>
+             
+         </tbody>
+      </table>
+    </div>
+</div>
+<script src="jquery-3.2.1.min.js"></script>
+<script src="bootstrap.min.js"></script>
+</body>
+</html>
